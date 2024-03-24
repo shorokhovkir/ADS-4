7226a7cf2793f34077d46383226f10938c9f0dc9
shorokhovkir <160240169+shorokhovkir@users.noreply.github.com>
2024-03-24 22:43:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,9 @@ int countPairs1(int *arr, int len, int value) {
   int cnt = 0;
   for (int i = 0; i < (len - 1); i++) {
     for (int j = i + 1; j < len; j++) {
-      if ((arr[i] + arr[j]) == value)
+      if ((arr[i] + arr[j]) == value) {
         cnt++;
+      }
     }
   }
   return cnt;
@@ -18,17 +19,22 @@ int countPairs2(int *arr, int len, int value) {
     }
   }
   for (int i = 0; i < (len - 1); i++) {
-    if (arr[i] > value)
+    if (arr[i] > value) {
       break;
-    if (k <= i)
+    }
+    if (k <= i) {
       j = i + 1;
-    else
+    }
+    else {
       j = k;
+    }
     for (; j < len; j++) {
-      if ((arr[i] + arr[j]) == value)
+      if ((arr[i] + arr[j]) == value) {
         cnt++;
-      if (arr[j] > value)
+      }
+      if (arr[j] > value) {
         break;
+      }
     }
   }
   return cnt;
@@ -36,51 +42,60 @@ int countPairs2(int *arr, int len, int value) {
 int countPairs3(int *arr, int len, int value) {
   int cnt = 0, k = 0, min = 0, max = 0;
   for (int i = 0; i < (len - 1); i++) {
-    if (arr[i] > value)
+    if (arr[i] > value) {
       break;
+    }
     min = i;
     max = len - 1;
     while (min < max) {
       k = min + ((max - min) / 2);
-      if (min == max)
+      if (min == max) {
         break;
+      }
       if ((arr[i] + arr[k]) > value) {
         for (int n = (k - 1); n >= min; n--) {
           max = n + 1;
-          if (arr[k] != arr[n])
+          if (arr[k] != arr[n]) {
             break;
+          }
         }
-        if (max - min == 1)
+        if (max - min == 1) {
           break;
+        }
       }
       if ((arr[i] + arr[k]) < value) {
         for (int n = (k + 1); n <= max; n++) {
           min = n - 1;
-          if (arr[k] != arr[n])
+          if (arr[k] != arr[n]) {
             break;
+          }
         }
-        if (max - min == 1)
+        if (max - min == 1) {
           break;
+        }
       }
       if ((arr[i] + arr[k]) == value) {
         int t = max;
         for (int n = (k + 1); n <= t; n++) {
           max = n - 1;
-          if (arr[k] != arr[n])
+          if (arr[k] != arr[n]) {
             break;
+          }
         }
         t = min;
         for (int n = (k - 1); n >= t; n--) {
           min = n + 1;
-          if (arr[k] != arr[n])
+          if (arr[k] != arr[n]) {
             break;
+          }
         }
         break;
       }
     }
     for (int j = min; j <= max; j++) {
-      if (((arr[i] + arr[j]) == value) && (i != j))
+      if (((arr[i] + arr[j]) == value) && (i != j)) {
         cnt++;
+      }
     }
   }
   return cnt;
